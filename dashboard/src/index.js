9bf96cc6ee26861f5fd966ded6aec1b855bb28e8
MykeDuong <hongminh4402@gmail.com>
2022-06-27 05:16:32 -0700

    Initialized the project

---
+++ dashboard/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+import './index.css';
+import App from './App';
+
+console.log(process.env.REACT_APP_SYNCFUSION_API)
+
+registerLicense(process.env.REACT_APP_SYNCFUSION_API);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+);
